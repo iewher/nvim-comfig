4606d97e33390a8439e522a2a093c352d498f0c9
iewher <goshagang@mail.ru>
2024-09-01 23:57:43 +0500

    chore: add cmp

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 -- Mason
 
 return {
-	--mason confug
+    -- mason конфигурация
     {
         "williamboman/mason.nvim",
         config = function()
@@ -9,7 +9,39 @@ return {
         end,
         lazy = false,
     },
-    	-- lsp config
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            'hrsh7th/cmp-nvim-lsp',  -- Поддержка LSP
+            'hrsh7th/cmp-buffer',     -- Автозаполнение из буфера
+            'hrsh7th/cmp-path',       -- Автозаполнение для путей
+            'hrsh7th/cmp-cmdline',    -- Автозаполнение для командной строки
+            'L3MON4D3/LuaSnip'        -- Сниппеты
+        },
+        config = function()
+            local cmp = require('cmp')
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end,
+                },
+                mapping = {
+                    ['<C-n>'] = cmp.mapping.select_next_item(),
+                    ['<C-p>'] = cmp.mapping.select_prev_item(),
+                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                },
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                    { name = 'cmdline' },
+                },
+            })
+        end
+    },
+    -- lsp конфигурация
     {
         "williamboman/mason-lspconfig.nvim",
         dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
@@ -19,10 +51,15 @@ return {
             }
 
             local lspconfig = require("lspconfig")
-		lspconfig.lua_ls.setup{}
-	    	lspconfig.tsserver.setup {}
-	    	lspconfig.gopls.setup {}
-	    	lspconfig.cssls.setup {}
+            local on_attach = function(_, bufnr)
+                local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+                buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+            end
+
+            lspconfig.lua_ls.setup { on_attach = on_attach }
+            lspconfig.tsserver.setup { on_attach = on_attach }
+            lspconfig.gopls.setup { on_attach = on_attach }
+            lspconfig.cssls.setup { on_attach = on_attach }
         end,
     },
 }
